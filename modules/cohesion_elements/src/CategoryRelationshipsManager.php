3ee7cc78a38f8dcc6e992cacbd4ec3ad5853e183
Jenkins <florent.metz@acquia.com>
2020-10-29 14:08:02 +0000

    version 6.3.4

---
+++ modules/cohesion_elements/src/CategoryRelationshipsManager.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\cohesion_elements;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Class CategoryRelationshipsManager.
+ *
+ * Handles resetting components and helpers with no category to a new "Uncategorized" category.
+ *
+ * @package Drupal\cohesion_elements
+ */
+class CategoryRelationshipsManager {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * CategoryRelationshipsManager constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * If the supplied category doesn't exist, scan for components / helpers that used it
+   * and convert them to use "Uncategorized" instead.
+   *
+   * @param $category_id
+   * @param $category_entity_type_id
+   * @param $category_entity_class
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function processCategory($category_id, $category_entity_type_id, $category_entity_class) {
+    $element_entity_type = $category_entity_class::target_entity_type;
+    $default_category_id = $category_entity_class::default_category_id;
+
+    // Setup.
+    $category_storage = $this->entityTypeManager->getStorage($category_entity_type_id);
+    $element_storage = $this->entityTypeManager->getStorage($element_entity_type);
+
+    // Is the category missing?
+    if (empty($category_storage->load($category_id)) && !empty($category_storage->loadMultiple())) {
+
+      // Is this category in use anywhere?
+      $query = $element_storage->getQuery()->condition('category', $category_id, '=');
+
+      if ($entity_ids = $query->execute()) {
+        // Does the uncategorized category exist?
+        // Create the uncategorized category.
+        if (!$category_storage->load($default_category_id)) {
+          $uncategorized = $category_storage->create([
+            'id' => $default_category_id,
+            'label' => t('Uncategorized'),
+            'class' => 'category-1',
+            'weight' => 999,
+          ]);
+
+          $uncategorized->save();
+        }
+
+        // Set all the elements to use this new category.
+        foreach ($element_storage->loadMultiple($entity_ids) as $element_entity) {
+          $element_entity->setCategory($default_category_id);
+          $element_entity->save();
+        }
+      }
+    }
+  }
+
+}
---
+++ tests/fixtures/import/drupal-8.9/expectations/cohesion/component_content.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @file
+ * Expectation for node with cohesion scenario.
+ */
+
+use Drupal\Tests\cohesion\Kernel\Stubs\CohesionCdfExpectations;
+
+$data = [
+  'uuid' => [
+    'en' => [
+      ['value' => 'ced8e818-f68a-4f7d-8408-6fbf61166874'],
+    ],
+  ],
+  'langcode' => [
+    'en' => [
+      ['value' => 'en'],
+    ],
+  ],
+  'revision_timestamp' => [
+    'en' => [
+      ['value' => '1584713128'],
+    ],
+  ],
+  'revision_uid' => [
+    'en' => [
+      ['target_id' => '47dcfb30-3c6e-46ce-990d-2632020849fc'],
+    ],
+  ],
+  'status' => [
+    'en' => [
+      ['value' => 1],
+    ],
+  ],
+  'title' => [
+    'en' => [
+      ['value' => 'ContentHubTestComponentContent'],
+    ],
+  ],
+  'uid' => [
+    'en' => [
+      ['target_id' => '47dcfb30-3c6e-46ce-990d-2632020849fc'],
+    ],
+  ],
+  'created' => [
+    'en' => [
+      ['value' => '1584713128'],
+    ],
+  ],
+  'changed' => [
+    'en' => [
+      ['value' => '1585134422'],
+    ],
+  ],
+  'layout_canvas' => [
+    'en' => [
+      ['target_id' => 'c255b7e7-5fef-4e24-83cf-fe0a777c5cfb'],
+    ],
+  ],
+  'component' => [
+    'en' => [
+      ['target_id' => '4361650b-c13e-42ce-9260-e5f9a8e31288'],
+    ],
+  ],
+  'default_langcode' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+  'revision_default' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+  'revision_translation_affected' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+];
+
+$expectations['ced8e818-f68a-4f7d-8408-6fbf61166874'] = new CohesionCdfExpectations($data, ['id', 'vid']);
+
+$data = [
+  'uuid' => [
+    'en' => [
+      ['value' => 'c255b7e7-5fef-4e24-83cf-fe0a777c5cfb'],
+    ],
+  ],
+  'langcode' => [
+    'en' => [
+      ['value' => 'en'],
+    ],
+  ],
+  'json_values' => [
+    'en' => [
+      ['value' => '{"canvas":[{"uid":"cpt_contenthubtestcomponent","type":"component","title":"ContentHubTestComponent","enabled":true,"category":"category-3","componentId":"cpt_contenthubtestcomponent","componentType":"heading","uuid":"90626630-68d9-4c22-951d-8559a7eca2a7","parentUid":"root","isContainer":0,"model":{"settings":{"title":"ContentHubTestComponentContent"}},"children":[]}],"model":{"90626630-68d9-4c22-951d-8559a7eca2a7":{"settings":{"title":"ContentHubTestComponentContent"}}},"mapper":null}'],
+    ],
+  ],
+  'parent_type' => [
+    'en' => [
+      ['value' => 'component_content']
+    ]
+  ],
+  'parent_field_name' => [
+    'en' => [
+      ['value' => 'layout_canvas']
+    ]
+  ],
+  'default_langcode' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+  'revision_default' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+  'revision_translation_affected' => [
+    'en' => [
+      ['value' => '1'],
+    ],
+  ],
+];
+
+$expectations['c255b7e7-5fef-4e24-83cf-fe0a777c5cfb'] = new CohesionCdfExpectations($data, ['id', 'revision', 'styles', 'template', 'last_entity_update', 'parent_id']);
+
+return $expectations;
