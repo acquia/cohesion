1b31beb0ea600f228aec9b0dada98eb1f576f704
James Wilson <jrwilson3@gmail.com>
2020-11-05 12:23:54 -0500

    Avoid Status Report warning: Tokens or token types missing name property

--- modules/cohesion_templates/src/Plugin/Field/FieldType/CohesionTemplateSelectorFieldItem.php
+++ modules/cohesion_templates/src/Plugin/Field/FieldType/CohesionTemplateSelectorFieldItem.php
@@ -50,7 +50,8 @@ class CohesionTemplateSelectorFieldItem extends FieldItemBase implements FieldIt
    * {@inheritdoc}
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-    $properties['selected_template'] = DataDefinition::create('string');
+    $properties['selected_template'] = DataDefinition::create('string')
+      ->setLabel(t('Selected template'));
     return $properties;
   }
 
