d1d520fb5abdeb63f9bcab5cc2ea3d276a008873
Jenkins <dev@cohesion.co.uk>
2021-01-27 14:32:12 +0000

    Merge pull request #300 from acquia/hotfix/ACO-797

---
+++ tests/src/Kernel/Stubs/CohesionCdfExpectations.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Tests\cohesion\Kernel\Stubs;
+
+use Drupal\cohesion\LayoutCanvas\LayoutCanvas;
+use Drupal\Tests\acquia_contenthub\Kernel\Stubs\CdfExpectations;
+
+/**
+ * Class CdfExpectations.
+ *
+ * @group Cohesion
+ *
+ * @package Drupal\Tests\acquia_contenthub\Kernel\Stubs
+ */
+class CohesionCdfExpectations extends CdfExpectations {
+
+  /**
+   * Return field value.
+   *
+   * @inheritDoc
+   */
+  public function getFieldValue(string $field_name, string $langcode = NULL) {
+
+    $field_value = parent::getFieldValue($field_name, $langcode);
+
+    if($field_name == 'json_values' && !empty($field_value) && isset($field_value[0]['value'])) {
+      $layout_canvas = new LayoutCanvas($field_value[0]['value']);
+      $field_value[0]['value'] = json_encode($layout_canvas);
+    }
+
+    return $field_value;
+  }
+
+}
