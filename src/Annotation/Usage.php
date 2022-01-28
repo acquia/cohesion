01429426e785a92e08763b94dbb2e037ea32bb72
Florent Metz <florent.metz@acquia.com>
2022-01-28 12:36:58 +0000

    Merge pull request #703 from acquia/6.4.x-to-6.5.x

---
+++ src/Annotation/Usage.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\cohesion\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines a reusable form plugin annotation object.
+ *
+ * @package Drupal\cohesion\Annotation;
+ *
+ * @Annotation
+ */
+class Usage extends Plugin {
+
+  /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+  /**
+   * The name of the form plugin.
+   *
+   * @var \Drupal\Core\Annotation\Translation
+   *
+   * @ingroup plugin_translatable
+   */
+  public $name;
+
+  /**
+   * The entity type that this plugin works for.
+   *
+   * @var string
+   */
+  public $entity_type;
+
+  /**
+   * The entity that this plugin works for can have dependencies (FALSE for
+   * things like WebsiteSettings colors and WebsiteSettings icons).
+   *
+   * @var bool
+   */
+  public $scannable;
+
+  /**
+   * The UsageUpdateManager can scan entities of the same type with the same
+   * plugin (nesting).
+   *
+   * @var bool
+   */
+  public $scan_same_type;
+
+  /**
+   * When grouping entities of this type on the package form, which entity
+   * key should be used to group by?
+   *
+   * This can be a comma separated list if there are more than one (ie. for
+   * content templates).
+   *
+   * @var string
+   */
+  public $group_key;
+
+  /**
+   * If group_key is a reference to an entity, what is it's type? If group_key
+   * does not reference an entity, put FALSE here.
+   *
+   * This can be a comma separated list if there are more than one (ie. for
+   * content templates).
+   *
+   * @var string
+   */
+  public $group_key_entity_type;
+
+  /**
+   * Does this plugin entity type appears as selectable on PackageForm?
+   *
+   * @var bool
+   */
+  public $exclude_from_package_requirements;
+
+  /**
+   * Should entities of this type be included in the full site export using sync?
+   *
+   * @var bool
+   */
+  public $exportable;
+
+  /**
+   * Is this a Cohesion or Core sync plugin?
+   * This is used to group Cohesion and core entities together in the package requirements on the sync package page.
+   *
+   * @var string
+   */
+  public $config_type;
+
+  /**
+   * Groups plugins to decide if they should be scanned for dependencies.
+   *
+   * @var array
+   */
+  public $scan_groups = [];
+
+}
